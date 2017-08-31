b0c4fed244d38468ebc5db00a5347eefe8dc2a92
Jore Rautava <jorerautava@gmail.com>
2017-08-31 09:32:37 +0300

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,11 @@
+/*
+ * main.c
+ *
+ *	This is main for my Bachelor's thesis work with RasPi and ADIS 16227 vibration sensor
+ *  Created on: 31.8.2017
+ *      Author: Jore
+ */
+
+int main(void) {
+	return 0;
+}
