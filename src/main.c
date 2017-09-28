43d9465ea43d6c8731fdc730b9dd5cd59c6a9305
Jore Rautava <jorerautava@gmail.com>
2017-09-28 19:14:25 +0300

    Yet Another Message Set

--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@
 int main() {
 	int i, fd;
 	struct axes *data_ptr, *tmp;
-	uint8_t tx[] = {REC_CTRL, 0x00};
+	uint8_t tx[] = {REC_CTRL, 0x00, 0x02, 0x32};
 	static const char *device = "/dev/spidev0.0";
 	static uint8_t mode = SPI_MODE_3;
 	static uint8_t bits = 8;
@@ -28,13 +28,13 @@ int main() {
 
 	while(i<10 && tmp != NULL) {
 		writeSPI(fd, tx);
-		tx[0] = X_BUF; tx[1] = 0x00;
+		tx[0] = X_BUF; tx[1] = 0x00; tx[2] = 0x00; tx[3] = 0x00;
 		data_ptr->x = readSPI(fd, tx);
-		tx[0] = Y_BUF; tx[1] = 0x00;
+		tx[0] = Y_BUF; tx[1] = 0x00; tx[2] = 0x00; tx[3] = 0x00;
 		data_ptr->y = readSPI(fd, tx);
-		tx[0] = Z_BUF;; tx[1] = 0x00;
+		tx[0] = Z_BUF;; tx[1] = 0x00; tx[2] = 0x00; tx[3] = 0x00;
 		data_ptr->z = readSPI(fd, tx);
-		tx[0] = REC_CTRL; tx[1] = 0x00;
+		tx[0] = REC_CTRL; tx[1] = 0x00; tx[2] = 0x02; tx[3] = 0x32;
 		sendToAzure(data_ptr);
 		i++;
 		memset(data_ptr, 0, sizeof(struct axes));
